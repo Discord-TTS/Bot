069c8ff8c99bb843d610e0b3ec3b4e3fbfa78cc8
TNTgamer71 <45660393+TNTgamer71@users.noreply.github.com>
2020-09-19 13:44:16 +0100

    Cleanup/remove some example code

--- main.py
+++ main.py
@@ -23,20 +23,17 @@ t = config["Main"]["Token"]
 
 #//////////////////////////////////////////////////////
 before = time.monotonic()
-OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+OPUS_LIBS = ('libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib')
 
 def load_opus_lib(opus_libs=OPUS_LIBS):
     if opus.is_loaded():
         return True
 
     for opus_lib in opus_libs:
-        try:
-            opus.load_opus(opus_lib)
-            return
-        except OSError:
-            pass
-
-        raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
+        try:    return opus.load_opus(opus_lib) 
+        except OSError: pass
+            
+        raise RuntimeError(f"Could not load an opus lib. Tried {', '.join(opus_libs)}")
 
 def listtostring1(s):
     str1 = ""
@@ -665,8 +662,6 @@ class Main(commands.Cog):
 class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.index = 0
-        self._last_member = None
 
     @commands.guild_only()
     @commands.bot_has_permissions(read_messages=True, send_messages=True, embed_links=True)
