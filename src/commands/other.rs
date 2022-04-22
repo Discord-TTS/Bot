690b39a10a063a5eb1499f6bdf5671dd8b6c38d0
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2022-04-22 14:55:21 +1000

    Added confirmation to /suggest

--- src/commands/other.rs
+++ src/commands/other.rs
@@ -18,8 +18,9 @@ use poise::serenity_prelude as serenity;
 use num_format::{Locale, ToFormattedString};
 
 use crate::constants::{OPTION_SEPERATORS};
-use crate::funcs::{fetch_audio, parse_user_or_guild, refresh_kind};
+use crate::funcs::{bool_button, fetch_audio, parse_user_or_guild, refresh_kind};
 use crate::structs::{Context, TTSMode, OptionTryUnwrap, CommandResult, PoiseContextExt};
+use crate::require;
 
 /// Shows how long TTS Bot has been online
 #[poise::command(category="Extra Commands", prefix_command, slash_command, required_bot_permissions="SEND_MESSAGES")]
@@ -204,6 +205,11 @@ pub async fn suggest(ctx: Context<'_>, #[description="the suggestion to submit"]
     if suggestion.to_lowercase().replace('<', ">") == "suggestion" {
         ctx.say("Hey! You are meant to replace `<suggestion>` with your actual suggestion!").await?;
         return Ok(())
+    }
+
+    if !require!(bool_button(ctx, format!("Are you sure you want to make a suggestion to {}?", ctx.discord().cache.current_user_field(|b| b.name.clone())).as_str(), "Yes", "Cancel", None).await?, Ok(())) {
+        ctx.say("Suggestion cancelled").await?;
+        return Ok(());
     }
 
     let data = ctx.data();
