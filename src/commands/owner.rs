f59bbdfa36389cd6b681c3f037e13177bfe1791e
Gnome <david2005thomas@gmail.com>
2022-04-25 18:52:41 +0100

    Add more info to -debug

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -101,26 +101,36 @@ pub async fn user_voice(ctx: Context<'_>, user: i64, mode: TTSModeServerChoice) 
 #[poise::command(prefix_command, guild_only, hide_in_help)]
 pub async fn debug(ctx: Context<'_>) -> CommandResult {
     let guild_id = ctx.guild_id().unwrap();
+    let guild_id_db: i64 = guild_id.into();
 
     let data = ctx.data();
     let ctx_discord = ctx.discord();
+    let author_id = ctx.author().id.into();
 
     let shard_id = ctx_discord.shard_id;
-    let guild_data = data.guilds_db.get(guild_id.into()).await?;
-    let user_data = data.userinfo_db.get(ctx.author().id.into()).await?;
-    let voice_client = songbird::get(ctx_discord).await.unwrap().get(guild_id);
+    let user_row = data.userinfo_db.get(author_id).await?;
+    let guild_row = data.guilds_db.get(guild_id_db).await?;
+    let nick_row = data.nickname_db.get([guild_id_db, author_id]).await?;
+    let guild_voice_row = data.guild_voice_db.get((guild_id_db, guild_row.voice_mode)).await?;
+    let user_voice_row = match user_row.voice_mode {
+        Some(mode) => Some(data.user_voice_db.get((author_id, mode)).await?),
+        None => None
+    };
 
+    let voice_client = songbird::get(ctx_discord).await.unwrap().get(guild_id);
     ctx.send(|b| {b.embed(|e| {e
         .title("TTS Bot Debug Info")
         .description(format!("
 Shard ID: `{shard_id}`
 Voice Client: `{voice_client:?}`
-Server Data: `{guild_data:?}`
-User Data: `{user_data:?}`
-        "))
-    })}).await?;
-    
-    Ok(())
+
+Server Data: `{guild_row:?}`
+User Data: `{user_row:?}`
+Nickname Data: `{nick_row:?}`
+User Voice Data: `{user_voice_row:?}`
+Guild Voice Data: `{guild_voice_row:?}`
+"))
+    })}).await.map(|_| ()).map_err(Into::into)
 }
 
 
