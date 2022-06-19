e85815c47e5f226d23ab98f38544f9af176fa1a7
Gnome <david2005thomas@gmail.com>
2022-06-19 15:53:51 +0100

    Make bool commands only optional in slash invoke

--- src/commands/settings.rs
+++ src/commands/settings.rs
@@ -511,22 +511,36 @@ macro_rules! create_bool_command {
         $description:literal,
         $value_desc:literal,
         $name:ident,
+        $cmd_name:literal,
         $key:literal,
         gettext($resp:literal),
         aliases($( $aliases:literal ),*),
         $($extra:tt)*
     ) => {
-        #[doc=$description]
-        #[poise::command(
-            category="Settings",
-            aliases($($aliases,)*),
-            required_permissions="ADMINISTRATOR",
-            required_bot_permissions="SEND_MESSAGES",
-            guild_only, prefix_command, slash_command,
-            $($extra)*
-        )]
-        pub async fn $name(ctx: Context<'_>, #[description=$value_desc] value: Option<bool>) -> CommandResult {
-            generic_bool_command(ctx, $key, value, $resp).await
+        pub fn $name() -> Command {
+            #[poise::command(prefix_command)]
+            pub async fn prefix_bool(ctx: Context<'_>, value: Option<bool>) -> CommandResult {
+                generic_bool_command(ctx, $key, value, $resp).await
+            }
+
+            #[doc=$description]
+            #[poise::command(
+                rename=$cmd_name,
+                category="Settings",
+                aliases($($aliases,)*),
+                guild_only, slash_command,
+                required_permissions="ADMINISTRATOR",
+                required_bot_permissions="SEND_MESSAGES",
+                $($extra)*
+            )]
+            pub async fn slash_bool(ctx: Context<'_>, #[description=$value_desc] value: bool) -> CommandResult {
+                generic_bool_command(ctx, $key, Some(value), $resp).await
+            }
+
+            Command {
+                prefix_action: prefix_bool().prefix_action,
+                ..slash_bool()
+            }
         }
     }
 }
@@ -534,39 +548,39 @@ macro_rules! create_bool_command {
 create_bool_command!(
     "Makes the bot say \"<user> said\" before each message",
     "Whether to say \"<user> said\" before each message",
-    xsaid, "xsaid",
+    xsaid, "xsaid", "xsaid",
     gettext("xsaid is now: {}"), aliases(),
 );
 create_bool_command!(
     "Makes the bot join the voice channel automatically when a message is sent in the setup channel",
     "Whether to automatically join voice channels",
-    autojoin, "auto_join",
+    autojoin, "autojoin", "auto_join",
     gettext("Auto Join is now: {}"), aliases("auto_join"),
 );
 create_bool_command!(
     "Makes the bot ignore messages sent by bots and webhooks",
     "Whether to ignore messages sent by bots and webhooks",
-    botignore, "bot_ignore",
+    botignore, "botignore", "bot_ignore",
     gettext("Ignoring bots is now: {}"), aliases("bot_ignore", "ignore_bots", "ignorebots"),
 );
 create_bool_command!(
     "Makes the bot require people to be in the voice channel to TTS",
     "Whether to require people to be in the voice channel to TTS",
-    require_voice, "require_voice",
+    require_voice, "require_voice", "require_voice",
     gettext("Requiring users to be in voice channel for TTS is now: {}"),
     aliases("voice_require", "require_in_vc"),
 );
 create_bool_command!(
     "Makes the bot ignore messages sent by members of the audience in stage channels",
     "Whether to ignore messages sent by the audience",
-    audience_ignore, "audience_ignore",
+    audience_ignore, "require_voice", "audience_ignore",
     gettext("Ignoring audience is now: {}"),
     aliases("audienceignore", "ignore_audience", "ignoreaudience"),
 );
 create_bool_command!(
     "Whether to use DeepL translate to translate all TTS messages to the same language ",
     "Whether to translate all messages to the same language",
-    translation, "to_translate",
+    translation, "translation", "to_translate",
     gettext("Translation is now: {}"), aliases("translate", "to_translate", "should_translate"),
     check="crate::premium_command_check",
 );
