40cc51e3b593be9858e834827b68b46caad6e49a
Gnome <david2005thomas@gmail.com>
2023-06-07 12:56:19 +0100

    Make auto leave less buggy

--- src/events/voice_state.rs
+++ src/events/voice_state.rs
@@ -4,22 +4,37 @@ use poise::serenity_prelude as serenity;
 use crate::structs::{Data, Result};
 
 
+/// If (on leave) the bot should also leave as it is alone
 pub async fn voice_state_update(ctx: &serenity::Context, data: &Data, old: Option<&serenity::VoiceState>, new: &serenity::VoiceState) -> Result<()> {
-    // If (on leave) the bot should also leave as it is alone
-    let bot_id = ctx.cache.current_user().id;
-    let guild_id = new.guild_id.try_unwrap()?;
+    // User left vc
+    let Some(old) = old else {return Ok(())};
 
-    if data.songbird.get(guild_id).is_some()
-        && let Some(old) = old && new.channel_id.is_none() // user left vc
-        && !new.member.as_ref().map_or(false, |m| m.user.id == bot_id) // user other than bot leaving
-        && !ctx.cache // filter out bots from members
-            .guild_channel(old.channel_id.try_unwrap()?)
-            .try_unwrap()?
-            .members(&ctx.cache)?
-            .into_iter().any(|m| !m.user.bot)
-    {
-        data.songbird.remove(guild_id).await?;
+    // Bot is in vc on server
+    let guild_id = new.guild_id.try_unwrap()?;
+    if data.songbird.get(guild_id).is_none() {
+        return Ok(());
+    }
+
+    // Bot is not the one leaving
+    let bot_id = ctx.cache.current_user().id;
+    if new.member.as_ref().map_or(true, |m| m.user.id == bot_id) {
+        return Ok(());
+    }
+
+    let channel_members = ctx.cache
+        .guild_channel(old.channel_id.try_unwrap()?)
+        .try_unwrap()?
+        .members(&ctx.cache)?;
+
+    // Bot is in the voice channel being left from
+    if channel_members.iter().all(|m| m.user.id != bot_id) {
+        return Ok(());
+    }
+
+    // All the users in the vc are now bots
+    if channel_members.into_iter().any(|m| !m.user.bot) {
+        return Ok(());
     };
 
-    Ok(())
+    data.songbird.remove(guild_id).await.map_err(Into::into)
 }
