ce42b84214157abef5005aa018b94a3f1b58c3eb
GnomedDev <david2005thomas@gmail.com>
2024-08-05 21:18:30 +0100

    Move the owner-only settings to a seperate file

--- tts_commands/src/settings/mod.rs
+++ tts_commands/src/settings/mod.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+mod owner;
 mod setup;
 mod voice_paginator;
 
@@ -453,72 +454,6 @@ fn check_prefix(prefix: &str) -> Result<ArrayString<5>, &'static str> {
 )]
 pub async fn set(ctx: Context<'_>) -> CommandResult {
     super::help::command(ctx, Some("set")).await
-}
-
-/// Owner only: used to block a user from dms
-#[poise::command(
-    prefix_command,
-    category = "Settings",
-    owners_only,
-    hide_in_help,
-    required_bot_permissions = "SEND_MESSAGES"
-)]
-pub async fn block(ctx: Context<'_>, user: serenity::UserId, value: bool) -> CommandResult {
-    ctx.data()
-        .userinfo_db
-        .set_one(user.into(), "dm_blocked", &value)
-        .await?;
-
-    ctx.say("Done!").await?;
-    Ok(())
-}
-
-/// Owner only: used to block a user from dms
-#[poise::command(
-    prefix_command,
-    category = "Settings",
-    owners_only,
-    hide_in_help,
-    required_bot_permissions = "SEND_MESSAGES"
-)]
-pub async fn bot_ban(ctx: Context<'_>, user: serenity::UserId, value: bool) -> CommandResult {
-    let user_id = user.into();
-    let userinfo_db = &ctx.data().userinfo_db;
-
-    userinfo_db.set_one(user_id, "bot_banned", &value).await?;
-    if value {
-        userinfo_db.set_one(user_id, "dm_blocked", &true).await?;
-    }
-
-    let msg = aformat!("Set bot ban status for user {user} to `{value}`.");
-    ctx.say(msg.as_str()).await?;
-
-    Ok(())
-}
-
-/// Owner only: Enables or disables the gTTS voice mode
-#[poise::command(
-    prefix_command,
-    category = "Settings",
-    owners_only,
-    hide_in_help,
-    required_bot_permissions = "SEND_MESSAGES"
-)]
-async fn gtts_disabled(ctx: Context<'_>, value: bool) -> CommandResult {
-    let data = ctx.data();
-    if data.config.gtts_disabled.swap(value, Ordering::Relaxed) == value {
-        ctx.say("It's already set that way, silly.").await?;
-        return Ok(());
-    }
-
-    let msg = if value {
-        "Disabled gTTS globally, womp womp"
-    } else {
-        "Re-enabled gTTS globally, yippee!\nMake sure to check the config file though."
-    };
-
-    ctx.say(msg).await?;
-    Ok(())
 }
 
 fn replace_bool(original: &str, value: bool) -> String {
@@ -1427,9 +1362,9 @@ pub fn commands() -> [Command; 5] {
                 require_voice(),
                 required_prefix(),
                 command_prefix(),
-                block(),
-                bot_ban(),
-                gtts_disabled(),
+                owner::block(),
+                owner::bot_ban(),
+                owner::gtts_disabled(),
                 use_new_formatting(),
             ],
             ..set()
