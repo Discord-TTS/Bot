089a261d265926560d6b464da4318b38479158b4
GnomedDev <david2005thomas@gmail.com>
2024-08-27 11:16:17 +0100

    Remove useless and inconsistent copyright headers

--- tts_core/src/constants.rs
+++ tts_core/src/constants.rs
@@ -1,19 +1,3 @@
-// Discord TTS Bot
-// Copyright (C) 2021-Present David Thomas
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 pub const RED: u32 = 0xff0000;
 pub const FREE_NEUTRAL_COLOUR: u32 = 0x3498db;
 pub const PREMIUM_NEUTRAL_COLOUR: u32 = 0xcaa652;
